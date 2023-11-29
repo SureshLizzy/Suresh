02e7d163114fc5ea229830727f11dbb640834f4e
SureshLizzy <sureshforjira@gmail.com>
2023-11-29 19:48:49 +0530

    initial codes

---
+++ src/test/java/com/marv/AvengerStones.java
@@ -0,0 +1,32 @@
+package com.marv;
+
+public class AvengerStones {
+	public void soulStone() {
+		System.out.println("Hawkeye");
+	}
+
+	public void mindStone() {
+		System.out.println("Vision");
+	}
+
+	public void powerStone() {
+		System.out.println("Star-Lord");
+	}
+
+	public void spaceStone() {
+		System.out.println("Loki");
+	}
+
+	public void timeStone() {
+		System.out.println("Dr.Strange");
+	}
+
+	public void realityStone() {
+		System.out.println("AsGuard");
+	}
+
+	private void wanda() {
+		System.out.println("Scarlet witch");
+	}
+
+}
